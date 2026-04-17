78963f06a9b6f9843828728c4dcf2f717fece751
aL1aL7 <info@aL1aL7.de>
2026-04-17 20:05:05 +0200

    Fix ignored custom fields at saved views

--- lib/models/saved_view.dart
+++ lib/models/saved_view.dart
@@ -33,56 +33,88 @@ class SavedView {
   String get ordering => sortReverse ? '-$sortField' : sortField;
 
   // Maps paperless-ngx filter_rule rule_type integers to API query params.
-  // Reference: paperless-ngx source documents/bulk_edit.py and filterrules.
+  // Reference: src-ui/src/app/data/filter-rule-type.ts (paperless-ngx main branch)
   Map<String, dynamic> toQueryParams() {
     final params = <String, dynamic>{};
-    final tagIds = <String>[];
-    final noTagIds = <String>[];
+    final tagsAll = <String>[];
+    final tagsIn = <String>[];
+    final tagsNone = <String>[];
+    final correspondentsIn = <String>[];
+    final correspondentsNone = <String>[];
+    final docTypesIn = <String>[];
+    final docTypesNone = <String>[];
+    final storagePathsIn = <String>[];
+    final storagePathsNone = <String>[];
+    final customFieldsAll = <String>[];
+    final customFieldsIn = <String>[];
+    final customFieldsNone = <String>[];
 
     for (final rule in filterRules) {
       final ruleType = rule['rule_type'] as int? ?? -1;
       final value = rule['value']?.toString();
 
       switch (ruleType) {
-        case 0: // title contains
-          params['title__icontains'] = value;
-        case 1: // content contains
-          params['content__icontains'] = value;
-        case 2: // ASN is
-          params['archive_serial_number__icontains'] = value;
-        case 3: // correspondent is
-          params['correspondent__id'] = value;
-        case 4: // document type is
-          params['document_type__id'] = value;
-        case 5: // is in inbox
-          params['is_in_inbox'] = true;
-        case 6: // has tag
-          if (value != null) tagIds.add(value);
-        case 7: // does not have tag
-          if (value != null) noTagIds.add(value);
-        case 8: // correspondent is not
-          params['correspondent__id__none'] = value;
-        case 9: // document type is not
-          params['document_type__id__none'] = value;
-        case 10: // created before
-          params['created__date__lt'] = value;
-        case 11: // created after
-          params['created__date__gt'] = value;
-        case 15: // added before
-          params['added__date__lt'] = value;
-        case 16: // added after
-          params['added__date__gt'] = value;
-        case 17: // title or content contains
-          params['query'] = value;
-        case 18: // fulltext query
-          params['query'] = value;
-        case 20: // has any of these tags
-          if (value != null) tagIds.add(value);
+        case 0: params['title__icontains'] = value;
+        case 1: params['content__icontains'] = value;
+        case 2: params['archive_serial_number'] = value;
+        case 3: params['correspondent__id'] = value;
+        case 4: params['document_type__id'] = value;
+        case 5: params['is_in_inbox'] = true;
+        case 6: if (value != null) tagsAll.add(value);
+        case 7: if (value != null) tagsIn.add(value);
+        case 8: params['created__date__lt'] = value;
+        case 9: params['created__date__gt'] = value;
+        case 10: params['created__year'] = value;
+        case 11: params['created__month'] = value;
+        case 12: params['created__day'] = value;
+        case 13: params['added__date__lt'] = value;
+        case 14: params['added__date__gt'] = value;
+        case 15: params['modified__date__lt'] = value;
+        case 16: params['modified__date__gt'] = value;
+        case 17: if (value != null) tagsNone.add(value);
+        case 18: params['archive_serial_number__isnull'] = true;
+        case 19: params['title_content'] = value;
+        case 20: params['query'] = value;
+        case 21: params['more_like_id'] = value;
+        case 22: if (value != null) tagsIn.add(value);
+        case 23: params['archive_serial_number__gt'] = value;
+        case 24: params['archive_serial_number__lt'] = value;
+        case 25: params['storage_path__id'] = value;
+        case 26: if (value != null) correspondentsIn.add(value);
+        case 27: if (value != null) correspondentsNone.add(value);
+        case 28: if (value != null) docTypesIn.add(value);
+        case 29: if (value != null) docTypesNone.add(value);
+        case 30: if (value != null) storagePathsIn.add(value);
+        case 31: if (value != null) storagePathsNone.add(value);
+        case 32: params['owner__id'] = value;
+        case 34: params['owner__isnull'] = true;
+        case 36: params['custom_fields__icontains'] = value;
+        case 38: if (value != null) customFieldsAll.add(value);
+        case 39: if (value != null) customFieldsIn.add(value);
+        case 40: if (value != null) customFieldsNone.add(value);
+        case 41: params['has_custom_fields'] = true;
+        case 42: params['custom_field_query'] = value;
+        case 43: params['created__date__lte'] = value;
+        case 44: params['created__date__gte'] = value;
+        case 45: params['added__date__lte'] = value;
+        case 46: params['added__date__gte'] = value;
+        case 47: params['mime_type'] = value;
       }
     }
 
-    if (tagIds.isNotEmpty) params['tags__id__all'] = tagIds.join(',');
-    if (noTagIds.isNotEmpty) params['tags__id__none'] = noTagIds.join(',');
+    if (tagsAll.isNotEmpty) params['tags__id__all'] = tagsAll.join(',');
+    if (tagsIn.isNotEmpty) params['tags__id__in'] = tagsIn.join(',');
+    if (tagsNone.isNotEmpty) params['tags__id__none'] = tagsNone.join(',');
+    if (correspondentsIn.isNotEmpty) params['correspondent__id__in'] = correspondentsIn.join(',');
+    if (correspondentsNone.isNotEmpty) params['correspondent__id__none'] = correspondentsNone.join(',');
+    if (docTypesIn.isNotEmpty) params['document_type__id__in'] = docTypesIn.join(',');
+    if (docTypesNone.isNotEmpty) params['document_type__id__none'] = docTypesNone.join(',');
+    if (storagePathsIn.isNotEmpty) params['storage_path__id__in'] = storagePathsIn.join(',');
+    if (storagePathsNone.isNotEmpty) params['storage_path__id__none'] = storagePathsNone.join(',');
+    if (customFieldsAll.isNotEmpty) params['custom_fields__id__all'] = customFieldsAll.join(',');
+    if (customFieldsIn.isNotEmpty) params['custom_fields__id__in'] = customFieldsIn.join(',');
+    if (customFieldsNone.isNotEmpty) params['custom_fields__id__none'] = customFieldsNone.join(',');
+
     params['ordering'] = ordering;
 
     return params;
